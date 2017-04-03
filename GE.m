ded5df0e117464958c4360d083938b74c653da87
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-04-03 12:19:02 +0200

    kod do poprawienia

--- GE.m
+++ GE.m
@@ -6,12 +6,12 @@ function [X, ex] = GE(A,b)
 %ex - b³¹d rozwi¹zania
 AB=[A,-b];
 n=size(A,1);
-W=1:n+1;%numery kolumn
+W=1:n;%numery kolumn
 for i=1:n-1
     k=i;
     %wyszukujemy element o najwiêkszym module
     for j=i+1:n
-        if abs(AB(i,W(k)))<abs(AB(i,W(j)))%to chyba da siê zrobiæ w jednej operacji
+        if abs(AB(W(k),i))<abs(AB(W(j),i))%to chyba da siê zrobiæ w jednej operacji
             k=j;
         end
     end
@@ -26,15 +26,16 @@ for i=1:n-1
         break;
     end
     for j=i+1:n
-        m=-AB(j,W(i))/AB(i,W(i));%wyznaczamy mno¿nik
-        for k=i+1:n+1 %sumujemy wiersz j-ty z wierszem i-tym przemno¿onym przez m
-            AB(j,W(k))=AB(j,W(k))+m*AB(i,W(k));%to chyba da siê zrobiæ w jednej operacji
+        m=-AB(W(j),i)/AB(W(i),i);%wyznaczamy mno¿nik
+        for kk=i:n+1 %sumujemy wiersz j-ty z wierszem i-tym przemno¿onym przez m
+            AB(W(j),kk)=AB(W(j),kk)+m*AB(W(i),kk);%to chyba da siê zrobiæ w jednej operacji
         end
     end
+    %AB(:,W(:))
 end
 % wyliczamy kolejne niewiadome
 for i=n:-1:1
-    if abs(AB(i,W(i)))<eps(0)
+    if abs(AB(W(i),i))<eps(0)
         %error('dzielnik zero\n')
         ex=Inf;
         %return;
@@ -42,9 +43,9 @@ for i=n:-1:1
     end
     s=AB(i,n+1);
     for j=n:-1:i+1
-        s=s-AB(i,W(j))*X(W(j));
+        s=s-AB(W(j),i)*X(W(j));
     end
-    X(W(i))=-s/AB(i,W(i));
+    X(W(i))=-s/AB(W(i),i);
 end
 %ex=n^3*2^(n-1)*eps;
 r=A*X'-b;
